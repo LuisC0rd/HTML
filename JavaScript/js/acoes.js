4c310f34de3a7651096b1c28ad9a29b81d493949
LuisC0rd <luis.gcsilva3@senacsp.edu.br>
2024-03-15 17:30:41 -0300

    atualização aula 15/03

--- JavaScript/js/acoes.js
+++ JavaScript/js/acoes.js
@@ -1,1 +1,8 @@
 'use strict'
+
+var azul = document.getElementById('azul')
+var verde = document.getElementById('verde')
+var laranja = document.getElementById('laranja')
+
+
+azul.onclick=document.body.style.backgroundColor='blue';
