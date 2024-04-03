ee26a70273c0c34330d233785292b553aa29770e
LuisC0rd <luis.gcsilva3@senacsp.edu.br>
2024-04-03 17:17:01 -0300

    Atualização 03/04

---
+++ JavaScript/js16.assets/js/aula07.js
@@ -0,0 +1,51 @@
+let vetor = []
+for (let posicao = 0; posicao < vetor.length; posicao++){
+    console.log(vetor[posicao])
+}
+
+let formContato = document.getElementById("form-contato");
+let divLista = document.getElementById("lista");
+let nome = [];
+let email = [];
+let telefone = [];
+let assunto = [];
+let msg = [];
+
+formContato.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    nome.push(formContato.nome.value);
+    email.push(formContato.email.value);
+    telefone.push(formContato.telefone.value);
+    assunto.push(formContato.assunto.value);
+    msg.push(formContato.mensagem.value);
+    gerarLista()
+})
+
+function gerarLista(){
+    let auxLista = "";
+
+    for (let posicao = 0; posicao <  nome.length; posicao++){
+        auxLista += `<div class="card">
+                        <div class="dados-label">
+                            <div>Nome Completo: </div>
+                            <div>${nome[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                            <div>E-mail: </div>
+                            <div>${email[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                            <div>Telefone: </div>
+                            <div>${telefone[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                            <div>Assunto: </div>
+                            <div>${assunto[posicao]}</div>
+                        </div>     
+                        <div class="dados-label">
+                            <div>Mensagem: </div>
+                            <div>${msg[posicao]}</div>
+                    </div>`
+
+                    divLista.innerHTML = auxLista
+}}
